929e175c23400a815878520e9af7845a8cb65afc
Vanessa819 <69302194+Vanessa819@users.noreply.github.com>
2020-11-13 16:33:02 +0100

    Add files via upload

---
+++ comparison_logical_operators/index.js
@@ -0,0 +1,60 @@
+const age= 18;
+
+
+if(age>18){
+    console.log("You can go to the Pub ");
+}else {
+    console.log("You can't go the Pub");
+}
+ const isFemale=true;
+  if (isFemale){
+      console.log( "She may enjoy that ladiesnight at the Pub");
+    
+  }else{
+      console.log("You can't go to that ladiesnight at the Pub");
+  }
+
+  const driverStatus = "bob";
+  
+  if (driverStatus == "bob"){
+      console.log("You're bob so you're allowed to drive");
+  }else{
+      console.log("You're not bob so you're not allowed to drive");
+  }
+
+  const myAge= 33;
+  
+  if (myAge>=18 && myAge<=25){
+      console.log("Je krijgt 50% korting");
+  }else{
+      console.log("Je krijgt helaas geen korting vandaag");
+  }
+
+
+  const myName ="Vanes";
+
+  if (myName ==="Sarah" || myName ==="Bram"){
+    console.log("Je heet Sarah of Bram dus gefeliciteerd!: je krijgt een gratis biertje");
+
+}else{
+    console.log("Helaas voor jou geen gratis bier!");
+
+}
+  
+const totalAmount=35;
+
+if (totalAmount >=  100) {
+    
+    console.log("gratis fles champagne");
+} else if (totalAmount > 50) {
+  
+    console.log("gratis portie nachos");
+} else if (totalAmount > 25) {
+  
+    console.log("gratis bitterballen");
+} else {
+  
+    console.log("Helaas niks gewonnen");
+}
+
+
